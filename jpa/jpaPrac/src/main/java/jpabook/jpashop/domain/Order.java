2fd44fdb7c79759042ae778eb87bf74198743b35
sungsikyang92 <sungsik.yang92@gmail.com>
2023-12-12 20:40:59 +0900

    itisworking

--- jpa/jpaPrac/src/main/java/jpabook/jpashop/domain/Order.java
+++ jpa/jpaPrac/src/main/java/jpabook/jpashop/domain/Order.java
@@ -18,16 +18,20 @@ public class Order {
     @JoinColumn(name="MEMBER_ID")
     private Member member;
 
+    @OneToOne
+    @JoinColumn(name = "DELIVERY_ID")
+    private Delivery delivery;
+
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
+
+    private LocalDateTime orderDate;
 
     public void addOrderItems(OrderItem orderItem) {
         orderItems.add(orderItem);
         orderItem.setOrder(this);
     }
 
-
-    private LocalDateTime orderDate;
 
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
